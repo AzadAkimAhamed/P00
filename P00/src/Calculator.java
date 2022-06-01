f052589a7b5eca9f3caffe05e5856099fa75ad5a
21016458 <21016458@DESKTOP-DD6TM0G>
2022-06-01 14:15:16 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,25 @@
+
+/**
+* I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21016458, 31 May 2022 11:14:47 pm
+ */
+public class Calculator {
+	public int add (int a, int b) {
+		return a+b;
+	}
+	
+	public int subtract (int a, int b) {
+		return a-b;
+	}
+	
+	public int multiple (int a, int b) {
+		return a*b;
+	}
+	
+	public int divide (int a, int b) {
+		return a/b;
+	}
+}
---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,73 @@
+
+/**
+* I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21016458, 31 May 2022 11:16:24 pm
+ */
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	// Arrange
+	int a = 4321;
+	int b = 1234;
+	int c = 0;
+	
+	Calculator cal = new Calculator();
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testAdd() {
+		// Act
+		int actual = cal.add(a, b);
+		
+		// Assert
+		int expected = 5555;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testSubtract() {
+		// Act
+		int actual = cal.subtract(a, b);
+		
+		// Assert 
+		int expected = 3087;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testMultiple() {
+		// Act 
+		int actual = cal.multiple(a, b);
+		
+		// Assert
+		int expected = 5332114;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testDivide() {
+		// Act
+		int actual = cal.divide(a, b);
+		
+		// Assert
+		int expected = 3;
+		assertEquals(expected, actual);
+	}
+	
+}
